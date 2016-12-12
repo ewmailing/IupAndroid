16e113ab227c02295bc9d51df8c6c6a9a52e4da7
Eric Wing <ewing -dot- public |-at-| gmail.com>
2016-12-12 13:19:39 -0800

    This demonstrates subclassing the IupApplication and IupLaunchActivity for Blurrr. Users will typically do similar things like this. I currently need these since I'm using the Blurrr SDK build system to get things going. I'm still not properly building a standalone libiup.so library and instead am rolling everything together into a large library. This needs to be fixed, but this shows how the getLibraries override can work.
    The java.in file is for Blurrr/CMake templating. Most users would just have the direct .java file.

---
+++ src/android/CMakeModules/AndroidGradle/app/src/main/java/com/blurrrsdk/app/BlurrrApplication.java
@@ -0,0 +1,18 @@
+package com.blurrrsdk.app;
+
+
+import android.app.Application;
+import android.view.View;
+import android.os.Bundle;
+//import android.content.res.AssetManager;
+import android.util.Log;
+
+import br.pucrio.tecgraf.iup.IupApplication;
+
+
+public class BlurrrApplication extends IupApplication
+{
+
+
+}
+
