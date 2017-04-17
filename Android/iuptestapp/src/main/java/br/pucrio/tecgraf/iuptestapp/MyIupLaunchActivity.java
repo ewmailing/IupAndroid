96fab8977a87b9ea5c9abe9db3bd31af7d474b91
Eric Wing <ewing -dot- public |-at-| gmail.com>
2017-04-17 16:58:42 -0700

    Android: New Android Studio project in working state.
    Every application will need to its own subclass of IupLaunchActivity and IupApplication and add yours to your manifest. (AAR Manifest merging should automatically add the IUP entries.)
    I am not enabling the res/anim animation raw assets yet because it may be a burden to integrate into projects that can't handle AAR.

---
+++ Android/iuptestapp/src/main/java/br/pucrio/tecgraf/iuptestapp/MyIupLaunchActivity.java
@@ -0,0 +1,26 @@
+package br.pucrio.tecgraf.iuptestapp;
+
+
+
+import android.app.Application;
+import android.view.View;
+import android.os.Bundle;
+//import android.content.res.AssetManager;
+import android.util.Log;
+
+import br.pucrio.tecgraf.iup.IupLaunchActivity;
+
+
+public class MyIupLaunchActivity extends IupLaunchActivity
+{
+	@Override
+	protected String[] getLibraries()
+	{
+		return new String[]
+				{
+						"MyIupProgram",
+						"iup",
+				};
+	}
+
+}
