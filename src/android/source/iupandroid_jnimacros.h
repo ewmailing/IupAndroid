70b5a7309c224fedcd1b8af6a5a5cbe63f82d6b9
Eric Wing <ewing -dot- public |-at-| gmail.com>
2018-09-08 05:09:41 -0700

    Android: Added a detailed example of how to use jnimacros and shows how the macros get transformed in the non-cached and cached cases.

--- src/android/source/iupandroid_jnimacros.h
+++ src/android/source/iupandroid_jnimacros.h
@@ -107,6 +107,151 @@ To use:
 		Since methodIDs don't get cleaned up in JNI, you don't need to do anything else.
 
 
+	
+///////////////////////////////////////////////////////////////////////////////			
+	Comparison:
+///////////////////////////////////////////////////////////////////////////////			
+
+///////////////////////////////////////////////////////////////////////////////			
+Macro JNI:
+	This is how you should use the Macros. 
+	Compare to the "Regular" version and see that it is almost 1-to-1,
+	so it should feel very natural to how you normally write JNI code.
+	Notice that you always clean up objects (not jmethodID's) 
+	with DeleteLocalRef when you are done. The expanded/cached version 
+	uses NewLocalRef to create temporary variables so things work out 
+	in either case.
+
+	Note: This is a simplified example. The real implementation is more complex due to needed featues.
+
+///////////////////////////////////////////////////////////////////////////////			
+
+#include <jni.h>
+#include "iupandroid_jnimacros.h"
+// #include "iupandroid_jnicacheglobals.h" // don't need for this example, but IupCommon is declared there.
+IUPJNI_DECLARE_CLASS_STATIC(IupProgressBarHelper);
+
+static int androidProgressBarMapMethod(Ihandle* ih)
+{	
+	JNIEnv* jni_env = iupAndroid_GetEnvThreadSafe();
+	jclass java_class = IUPJNI_FindClass(IupLabelHelper, jni_env, "br/pucrio/tecgraf/iup/IupLabelHelper");
+
+	IUPJNI_DECLARE_METHOD_ID_STATIC(IupProgressBarHelper_createProgressBar);
+
+	jmethodID method_id = IUPJNI_GetStaticMethodID(IupProgressBarHelper_createProgressBar, jni_env, java_class, "createProgressBar", "(J)Landroid/widget/ProgressBar;");
+
+	jobject java_widget = (*jni_env)->CallStaticObjectMethod(jni_env, java_class, method_id, (jlong)(intptr_t)ih);
+
+	// In the cached case, this will clean up a NewLocalRef copy that the was created so it is consistent with the non-cached case.
+	(*jni_env)->DeleteLocalRef(jni_env, java_class);
+
+
+
+	// Below is not part of macro example, but just for completeness of the function.
+	ih->handle = (jobject) ((*jni_env)->NewGlobalRef(jni_env, java_widget));
+	(*jni_env)->DeleteLocalRef(jni_env, java_widget);
+	// add to the parent, all Android controls must call this.
+	iupAndroid_AddWidgetToParent(jni_env, ih);
+	return IUP_NOERROR;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////			
+	Expanded/Transformed Non-Cached JNI (aka Regular JNI):
+	This is the most straight-forward JNI code, which most people would write.
+	Notice that the DECLARE macros get eliminated so there are no static variables.
+///////////////////////////////////////////////////////////////////////////////			
+
+
+#include <jni.h>
+
+static int androidProgressBarMapMethod(Ihandle* ih)
+{
+	JNIEnv* jni_env = iupAndroid_GetEnvThreadSafe();
+
+	jobject java_class = (*jni_env)->FindClass(jni_env, "br/pucrio/tecgraf/iup/IupProgressBarHelper");
+
+	jmethodID method_id = (*jni_env)->GetStaticMethodID(jni_env, java_class, "createProgressBar", "(J)Landroid/widget/ProgressBar;");
+
+	jobject java_widget = (*jni_env)->CallStaticObjectMethod(jni_env, java_class, method_id, (jlong)(intptr_t)ih);
+
+	(*jni_env)->DeleteLocalRef(jni_env, java_class);
+
+
+
+	// Below is not part of macro example, but just for completeness of the function.
+	ih->handle = (jobject) ((*jni_env)->NewGlobalRef(jni_env, java_widget));
+	(*jni_env)->DeleteLocalRef(jni_env, java_widget);
+	// add to the parent, all Android controls must call this.
+	iupAndroid_AddWidgetToParent(jni_env, ih);
+	return IUP_NOERROR;
+}
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////			
+Expanded/Transformed Cached JNI:
+	If caching is enabled, then the Macros are expanded to this.
+///////////////////////////////////////////////////////////////////////////////			
+
+#include <jni.h>
+static jobject g_javaClassIupProgressBarHelper = NULL;
+
+static int androidProgressBarMapMethod(Ihandle* ih)
+{
+	JNIEnv* jni_env = iupAndroid_GetEnvThreadSafe();
+
+	jobject java_class;
+	{
+		jobject tmp_jclass_IupProgressBarHelper = NULL;
+		if(NULL == g_javaClassIupProgressBarHelper)
+		{
+			tmp_jclass_IupProgressBarHelper = (*jni_env)->FindClass(jni_env, "br/pucrio/tecgraf/iup/IupProgressBarHelper");
+			g_javaClassIupProgressBarHelper = (jobject)((*jni_env)->NewGlobalRef(jni_env, tmp_jclass_IupProgressBarHelper));
+		}
+		else
+		{
+			tmp_jclass_IupProgressBarHelper = (jobject)((*jni_env)->NewLocalRef(jni_env, g_javaClassIupProgressBarHelper));
+		}
+		java_class = tmp_jclass_IupProgressBarHelper;
+	}
+
+
+
+	static jmethodID s_methodID_IupProgressBarHelper_createProgressBar = 0;
+	jmethodID method_id;
+	{
+		if(0 == s_methodID_IupProgressBarHelper_createProgressBar)
+		{
+			method_id = (*jni_env)->GetStaticMethodID(jni_env, java_class, "createProgressBar", "(J)Landroid/widget/ProgressBar;");
+			s_methodID_IupProgressBarHelper_createProgressBar = method_id;
+		}
+		else
+		{
+			method_id = s_methodID_IupProgressBarHelper_createProgressBar;
+		}
+	}
+
+
+	jobject java_widget = (*jni_env)->CallStaticObjectMethod(jni_env, java_class, method_id, (jlong)(intptr_t)ih);
+
+	(*jni_env)->DeleteLocalRef(jni_env, java_class);
+
+
+
+	// Below is not part of macro example, but just for completeness of the function.
+	ih->handle = (jobject) ((*jni_env)->NewGlobalRef(jni_env, java_widget));
+	(*jni_env)->DeleteLocalRef(jni_env, java_widget);
+	// add to the parent, all Android controls must call this.
+	iupAndroid_AddWidgetToParent(jni_env, ih);
+	return IUP_NOERROR;
+}
+
+
+
+
 */
 
 
